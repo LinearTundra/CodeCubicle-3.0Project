4e3d057dcc0d24bd83b49e9f889313ea299a4c5a
LinearTundra <samdarshmahajan123@gmail.com>
2024-09-15 12:58:10 +0530

    searchtext

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
-const SearchText=document.getElementById("Search");
-const searchbutton=document.getElementById("SearchButton")
+const SearchText = document.getElementById("Search");
+const searchbutton = document.getElementById("SearchButton")
 searchbutton.addEventListener("click",function(){
     console.log(SearchText.value)
+    document.getElementById("SearchResultText").textContent = SearchText;
 })
 
 
