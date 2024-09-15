54889b86116f36c310a05d51a57132893b951731
LinearTundra <samdarshmahajan123@gmail.com>
2024-09-15 13:27:51 +0530

    Revert "input"
    This reverts commit cf7f2dd89ff1534a60710738f6f1d5f43192740d.

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 
-const SearchText = document.getElementById("Search") ;
-const searchbutton = document.getElementById("SearchButton") ;
+const SearchText = document.getElementById("Search");
+const searchbutton = document.getElementById("SearchButton")
 searchbutton.addEventListener("click",function(){
-    console.log(SearchText.value) ;
-    document.getElementById("SearchResultText").textContent = SearchText.value ;
-})
-SearchText.addEventListener("keyup", (e) => {
-    if (e === 13) {
-        document.getElementById("SearchResultText").textContent = SearchText.value ;
-    }
+    console.log(SearchText.value)
+    document.getElementById("SearchResultText").textContent = SearchText;
 })
 
 
