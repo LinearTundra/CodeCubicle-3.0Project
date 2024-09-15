cf7f2dd89ff1534a60710738f6f1d5f43192740d
LinearTundra <samdarshmahajan123@gmail.com>
2024-09-15 13:25:12 +0530

    input

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 
-const SearchText = document.getElementById("Search");
-const searchbutton = document.getElementById("SearchButton")
+const SearchText = document.getElementById("Search") ;
+const searchbutton = document.getElementById("SearchButton") ;
 searchbutton.addEventListener("click",function(){
-    console.log(SearchText.value)
-    document.getElementById("SearchResultText").textContent = SearchText;
+    console.log(SearchText.value) ;
+    document.getElementById("SearchResultText").textContent = SearchText.value ;
+})
+SearchText.addEventListener("keyup", (e) => {
+    if (e === 13) {
+        document.getElementById("SearchResultText").textContent = SearchText.value ;
+    }
 })
 
 
